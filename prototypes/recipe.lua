96cc458eac3bf7299cd3178c79635c49be32655c
Anthony Poschen <zanven42@gmail.com>
2025-04-07 22:21:24 +1000

    fix: Formatting issues

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -1,12 +1,13 @@
 data:extend({
-  {
-    	type = "recipe",
-    	name = "wooden-wall",
+	{
+		type = "recipe",
+		name = "wooden-wall",
 		icon = "__Wood-Walls__/graphics/wooden-wall/wooden-wall.png",
 		icon_size = 32,
 		enabled = true,
-    	ingredients = {	{type="item", name="wood", amount=2}},
-		results = {{type="item",name="wooden-wall",amount=2}},
+		ingredients = { { type = "item", name = "wood", amount = 2 } },
+		results = { { type = "item", name = "wooden-wall", amount = 2 } },
 		energy_required = 1,
-  }
-  })
+	},
+})
+
